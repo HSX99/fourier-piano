c385a9876c3de40d96859cd4f767ae6eca4280ab
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-05 02:18:00 +0100

    fft functinoailty

---
+++ single_sided_spectrum.m
@@ -0,0 +1,12 @@
+function [F, X] = single_sided_spectrum(x,fs,timeSpan)
+%SINGLE_SIDED_SPECTRUM Summary of this function goes here
+%   Detailed explanation goes here
+% Fouriertransform
+nSamples = fs*timeSpan;
+df = fs/nSamples;   % resolution in frequency
+fSpan = df:df:fs;
+F = fSpan(1:end/2);
+X_full = abs(fft(x));
+X = X_full(1:length(F));    % only use half of the fft
+end
+
