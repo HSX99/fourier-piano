d0d7cb9f8d03b75a8f43e1ad602186fee0f0e050
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-06 16:05:55 +0100

    cleanup

--- plot_piano.m
+++ plot_piano.m
@@ -1,16 +1,14 @@
-function [] = plot_piano(frequencies,powerSpectrum,maxFFT,limits)
+function [] = plot_piano(frequencies,fftVec,fftMax,freqLimits)
 %PLOT_PIANO Summary of this function goes here
 %   Detailed explanation goes here
 
 [notes,whiteKeysIndexes,blackKeysIndexes] = piano_info();
 
-% Plot Piano
-% A = 1.2 .* max(powerSpectrum);
-A = 1.2*maxFFT;
+A = 1.2*fftMax;
 clf
 hold on
-f_min = limits(1);
-f_max = limits(2);
+f_min = freqLimits(1);
+f_max = freqLimits(2);
 
 for i = 1+1:60-1
     index = whiteKeysIndexes(i);
@@ -33,16 +31,14 @@ for i = 1+1:42-1
 
 end
 
-plot(log(frequencies),powerSpectrum,'color','r', 'Linewidth', 3)
-
-% plot(log(frequencies),powerSpectrum, 'linewidth',2)
+plot(log(frequencies),fftVec,'color','r', 'Linewidth', 3)
 
 xlim(log([f_min, f_max]))
 ylim([0, A])
 
-c_octaves = log(16.4.*(2.^(1:6))); % octaves of C
+c_octaves = log(16.4.*(2.^(1:8))); % octaves of C
 xticks(c_octaves) 
-xticklabels({'C1','C2','C3','C4','C5','C6'})
+xticklabels({'C1','C2','C3','C4','C5','C6','C7','C8'})
 yticks([])
 
 x0=100;y0=100;width=2*550;height=400;
