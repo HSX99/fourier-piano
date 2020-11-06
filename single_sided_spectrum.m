18a338524e7ef78123f916b6720b598f640956d6
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-06 16:06:21 +0100

    clean uo

--- single_sided_spectrum.m
+++ single_sided_spectrum.m
@@ -1,12 +1,8 @@
 function [F, X] = single_sided_spectrum(x,fs,timeSpan)
-%SINGLE_SIDED_SPECTRUM Summary of this function goes here
-%   Detailed explanation goes here
-% Fouriertransform
-nSamples = fs*timeSpan;
-df = fs/nSamples;   % resolution in frequency
-fSpan = df:df:fs;
-F = fSpan(1:end/2);
-%X_full = 20*log(abs(fft(x)));   %
+nSamples = fs*timeSpan;     % number of recorded samples
+df = fs/nSamples;           % frequency resolution
+fSpan = df:df:fs;           
+F = fSpan(1:end/2);         % all frequencies represented in the fft
 X_full = abs(fft(x));
 X = X_full(1:length(F));    % only use half of the fft
 end
