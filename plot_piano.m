bc49a633d772315da083b1f222cf757db3e19eaf
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 17:21:09 +0100

    analytic expression for key edges

--- plot_piano.m
+++ plot_piano.m
@@ -1,55 +1,55 @@
-function [] = plot_piano(frequencies,fftVec,fftMax,freqLimits,nOctaves)
-%PLOT_PIANO Summary of this function goes here
-%   Detailed explanation goes here
+function [] = plot_piano(maxFFT,low,high)
+%PLOT_PIANO
+NOTE_ABOVE_RATIO = 2^(1/12);    % factor of adjecent keys
+NOTE_BELOW_RATIO = 2^(-1/12);
+HALFWAY_ABOVE_FACTOR = (1 + NOTE_ABOVE_RATIO)/2;
+HALFWAY_BELOW_FACTOR = (1 + NOTE_BELOW_RATIO)/2;
+f_min = 16.4.*(2.^low)*HALFWAY_BELOW_FACTOR;
+f_max = 16.4.*(2.^high)*HALFWAY_ABOVE_FACTOR;
+
 
 [notes,whiteKeysIndexes,blackKeysIndexes] = piano_info();
+A = 1.2*maxFFT;
 
-A = 1.2*fftMax;
-clf
-hold on
-f_min = freqLimits(1);
-f_max = freqLimits(2);
-
-for i = 1+1:60-1
-    index = whiteKeysIndexes(i);
-    note = notes(index);
+% plot the white keys
+for i = 1:length(whiteKeysIndexes)
+    
+    note = notes(whiteKeysIndexes(i));
+    
     if (note > f_min) && (note < f_max)
-        beginKey = mean([notes(index-1), note]);
-        endKey = mean([note, notes(index+1)]);
-        area(log([beginKey, endKey]), [A, A], 'FaceColor', 'w')
+        area(log(note.*[HALFWAY_BELOW_FACTOR, HALFWAY_ABOVE_FACTOR]), [A, A], 'FaceColor', 'w')
     end
 end
 
-for i = 1+1:42-1
-    index = blackKeysIndexes(i);
-    note = notes(index);
+% plot the black keys
+for i = 1:length(blackKeysIndexes)
+    
+    note = notes(blackKeysIndexes(i));
+    
     if (note >= f_min) && (note <= f_max)
-        beginKey = mean([notes(index-1), note]);
-        endKey = mean([note, notes(index+1)]);
-        area(log([beginKey, endKey]), [A, A], 0, 'FaceColor', 'k')
+        area(log(note.*[HALFWAY_BELOW_FACTOR, HALFWAY_ABOVE_FACTOR]), [A, A], 0, 'FaceColor', 'k')
     end
 
 end
 
-plot(log(frequencies),fftVec,'color','r', 'Linewidth', 3)
+% set limits, tick label font size and figure size
 
-xlim(log([f_min, f_max]))
+xlim(log([f_min^(0.999), f_max^(1/0.9999)]))    %
 ylim([0, A])
 
-c_octaves = log(16.4.*(2.^(1:8))); % octaves of C
+c_octaves = log(16.4.*(2.^(0:8))); % octaves of C
 xticks(c_octaves) 
-xticklabels({'C1','C2','C3','C4','C5','C6','C7','C8'})
+xticklabels({'C0','C1','C2','C3','C4','C5','C6','C7','C8'})
 yticks([])
-
-x0=100;y0=100;
-width=550*nOctaves;
-height=400;
 
 a = get(gca,'XTickLabel');
 set(gca,'XTickLabel',a,'fontsize',16)
 
+x0=100;
+y0=100;
+width=min([550*2,550*(high-low)]);
+height=400;
 set(gcf,'units','points','position',[x0,y0,width,height])
-hold off
 
 end
 
