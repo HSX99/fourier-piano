7231adaf0fcfb7bea16ad57fae77c9837eec4689
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 17:31:21 +0100

    index fixing

--- split2windows.m
+++ split2windows.m
@@ -2,18 +2,21 @@ function [F,allFFTWindows,nWindows] = split2windows(x,fs,timeWindow,windowOverla
 nSamples = length(x);
 nSamplesWindow = round(fs*timeWindow/2)*2; % even number of samples
 nWindows = floor((nSamples-(nSamplesWindow*windowOverlap))/(nSamplesWindow*(1-windowOverlap)));
-nSamples = ((nWindows-1)*(1-windowOverlap) + 1)*nSamplesWindow + 1; % update length of x
+
+nSamples = round(((nWindows-1)*(1-windowOverlap) + 1)*nSamplesWindow + 1); % update length of x, NEEDS FIXING??
+
 x = x(1:nSamples);
 
+% allWindows = nan(nWindows,nSamplesWindow);
 allFFTWindows = nan(nWindows,nSamplesWindow/2); % allocate
 for i = 1:nWindows
-    % disp([num2str(i), '/', num2str(nWindows)])
     a = floor((i-1)*nSamplesWindow*(1-windowOverlap)+1);
     b = a + nSamplesWindow - 1;
     xWindowed = x(a:b).*hamming(nSamplesWindow);
         
     [F,X] = single_sided_spectrum(xWindowed,fs,timeWindow);
-        
+    
+    % allWindows(i,:) = xWindowed;
     allFFTWindows(i,:) = X;
 end
 
