71af1971daeb6853bc14e0e44d5f0f2207fdfe43
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 17:21:41 +0100

    improved handling of static piano backdrop

--- make_gif.m
+++ make_gif.m
@@ -1,17 +1,28 @@
-function [] = make_gif(h,F,X,maxFFT,limits,nOctaves,i,filename)
+function [] = make_gif(F,allFFTWindows,maxFFT,c_low,c_high,filename)
+[nWindows, ~] = size(allFFTWindows);
+
+h = figure;
+hold on
 axis tight manual % this ensures that getframe() returns a consistent size
 
-plot_piano(F,X,maxFFT,limits,nOctaves)
-drawnow 
-% Capture the plot as an image 
-frame = getframe(h); 
-im = frame2im(frame); 
-[imind,cm] = rgb2ind(im,256); 
-% Write to the GIF File 
-if i == 1 
-  imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
-else 
-  imwrite(imind,cm,filename,'gif','WriteMode','append'); 
-end 
+plot_piano(maxFFT,c_low,c_high);    % plot piano backdrop
+
+for i = 1:nWindows
+    X = allFFTWindows(i,:);
+    fft_win = plot(log(F),X,'color','r', 'Linewidth', 3);
+    drawnow 
+    % Capture the plot as an image 
+    frame = getframe(h); 
+    im = frame2im(frame); 
+    [imind,cm] = rgb2ind(im,256); 
+    % Write to the GIF File 
+    if i == 1 
+      imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+    else 
+      imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+    end 
+    delete(fft_win)     % delete current fft from plot
+end
+hold off
 
 end
