2bdfb94c383db2318fb670afa49003ce04f9127e
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 12:40:31 +0100

    indexing error fixed

--- split2windows.m
+++ split2windows.m
@@ -1,23 +1,19 @@
-function [F,allFFTWindows,nWindows] = split2windows(x, fs)
-timeWindow = 0.4;       % seconds per window
-windowOverlap = 0.3;    % overlap between consecutive windows (0 = no overlap, etc.)
-
+function [F,allFFTWindows,nWindows] = split2windows(x,fs,timeWindow,windowOverlap)
 nSamples = length(x);
-
-nSamplesWindow = round(fs*timeWindow/2)*2; %
-
+nSamplesWindow = round(fs*timeWindow/2)*2; % even number of samples
 nWindows = floor((nSamples-(nSamplesWindow*windowOverlap))/(nSamplesWindow*(1-windowOverlap)));
-
-nSamples = ((nWindows-1)*(1-windowOverlap) + 1)*nSamplesWindow; % update length of x
+nSamples = ((nWindows-1)*(1-windowOverlap) + 1)*nSamplesWindow + 1; % update length of x
 x = x(1:nSamples);
 
-allFFTWindows = nan(nWindows,nSamplesWindow/2);
-
+allFFTWindows = nan(nWindows,nSamplesWindow/2); % allocate
 for i = 1:nWindows
+    % disp([num2str(i), '/', num2str(nWindows)])
     a = floor((i-1)*nSamplesWindow*(1-windowOverlap)+1);
     b = a + nSamplesWindow - 1;
     xWindowed = x(a:b).*hamming(nSamplesWindow);
+        
     [F,X] = single_sided_spectrum(xWindowed,fs,timeWindow);
+        
     allFFTWindows(i,:) = X;
 end
 
