412dc17577bda016498f26c5e368d4d7a48e7c8f
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 17:31:31 +0100

    cleanup

--- main.m
+++ main.m
@@ -25,26 +25,20 @@ windowOverlap = 0.3;    % overlap between consecutive windows (0 = no overlap, e
 low_c = 4;  % lowest c shown on the piano
 high_c = 5; % highest c shown on the piano
 
+
 %%
 sound(x,fs)
 
 %%
-
 [F, allFFTWindows,nWindows] = split2windows(x, fs, timeWindow,windowOverlap); % divide signal into windows and perform FFT on each window
-nOctaves = high_c - low_c;
-delta = 0.51; %  extra space to show the left and rightmost keys completely on the piano
-C0 = 16.4;  % frequency of bottom C
-freq_limits = [(C0-delta)*2.^low_c, (C0+delta)*2.^high_c];
 
-maxFFT = max_in_range(allFFTWindows,F,freq_limits); % used for scaling the piano background
+maxFFT = max_in_range(allFFTWindows,F,low_c,high_c); % used for scaling the piano background
+%%
+display_sequence(F,allFFTWindows,maxFFT,low_c,high_c)
 
-h = figure;
-% filename = 'gifs/intel.gif';
+%%
+make_gif(F,allFFTWindows,maxFFT,low_c,high_c,'test-new.gif')
 
-for i = 1:nWindows
-    X = allFFTWindows(i,:);
-    plot_piano(F,X,maxFFT,freq_limits,nOctaves)
-    % make_gif(h,F,X,maxFFT,freq_limits,nOctaves,i,filename);
-    pause(0.01)
-end
+
+
 
