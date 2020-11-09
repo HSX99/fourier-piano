a699164fdbbebde35682bacc2af08957e0c22231
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 17:22:23 +0100

    works with octave ranges instead of frequencies

--- max_in_range.m
+++ max_in_range.m
@@ -1,8 +1,14 @@
-function maxFFT = max_in_range(allFFTWindows,F, limits)
+function maxFFT = max_in_range(allFFTWindows,F,low,high)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-above = F > limits(1); 
-below = F < limits(2);
+NOTE_ABOVE_RATIO = 2^(1/12);    % factor of adjecent keys
+NOTE_BELOW_RATIO = 2^(-1/12);
+HALFWAY_ABOVE_FACTOR = (1 + NOTE_ABOVE_RATIO)/2;
+HALFWAY_BELOW_FACTOR = (1 + NOTE_BELOW_RATIO)/2;
+f_min = 16.4.*(2.^low)*HALFWAY_BELOW_FACTOR;
+f_max = 16.4.*(2.^high)*HALFWAY_ABOVE_FACTOR;
+above = F > f_min; 
+below = F < f_max;
 inPianoRange = bitand(above, below);
 allFFTWindows__ = allFFTWindows(:,inPianoRange==1);
 maxFFT = max(allFFTWindows__(:));
