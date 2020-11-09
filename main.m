a5b4604473ef29f0a70d3630c63f5ee26b2b1af3
torstenmolitor <torstenmolitor@hotmail.com>
2020-11-09 13:02:55 +0100

    main script update

--- main.m
+++ main.m
@@ -20,11 +20,13 @@ high_c = 7; % highest c shown on the piano
 %% intel
 [x,fs] = audioread('sounds/intel-sound-logo.mp3');
 x = mean(x,2);  % naive stereo -> mono
-% sound(x,fs)
 timeWindow = 0.12;       % seconds per window
 windowOverlap = 0.3;    % overlap between consecutive windows (0 = no overlap, etc.)
 low_c = 4;  % lowest c shown on the piano
 high_c = 5; % highest c shown on the piano
+
+%%
+sound(x,fs)
 
 %%
 
